faaa5b4dfaa18bf3dff46a55d5366d227fcc1e34
yismaeel21 <52178055+yismaeel21@users.noreply.github.com>
2020-04-02 10:57:21 -0700

    Update BinaryTree.py

--- Trees/BinaryTree.py
+++ Trees/BinaryTree.py
@@ -139,7 +139,7 @@ class BinaryTree():
         Implement this function by modifying the _print functions above.
         '''
         if start:
-            traversal += start.value # + "-")
+            traversal += int(start.value) # + "-")
             traversal = self.preorder(start.left, traversal)
             traversal = self.preorder(start.right, traversal)
         return traversal
